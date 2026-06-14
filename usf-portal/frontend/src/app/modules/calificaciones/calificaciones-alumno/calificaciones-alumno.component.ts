a2921f7d6c20f0c99d5c8f964f7bf1cbf4bd0bea
LuisSuaEsq <tu_email@gmail.com>
2026-06-13 23:38:14 -0600

    feat: agregar vista de calificaciones para alumno con estilos

---
+++ usf-portal/frontend/src/app/modules/calificaciones/calificaciones-alumno/calificaciones-alumno.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ApiService } from '../../../core/services/api.service';
+import { AuthService } from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-calificaciones-alumno',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './calificaciones-alumno.component.html',
+  styleUrls: ['./calificaciones-alumno.component.scss'],
+})
+export class CalificacionesAlumnoComponent implements OnInit {
+  private readonly apiService = inject(ApiService);
+  private readonly authService = inject(AuthService);
+
+  calificaciones: any[] = [];
+  cargando = true;
+  error = '';
+
+  ngOnInit(): void {
+    this.cargarCalificaciones();
+  }
+
+  cargarCalificaciones(): void {
+    this.apiService.get<any[]>('calificaciones/mis-calificaciones').subscribe({
+      next: (cals) => {
+        this.calificaciones = cals;
+        this.cargando = false;
+      },
+      error: () => {
+        this.error = 'No se pudieron cargar las calificaciones.';
+        this.cargando = false;
+      }
+    });
+  }
+
+  cerrarSesion(): void {
+    this.authService.logout();
+  }
+}
